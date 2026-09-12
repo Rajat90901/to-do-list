33e696b0e12564ef093c32b2d5904e1b0f0c83bd
Rajat Chauhan <rajatchauhan9090@gmail.com>
2026-09-12 21:57:59 +0530

    added basic cli logic

--- todo.js
+++ todo.js
@@ -2,31 +2,19 @@ const process=require('process');
 const command=process.argv[2];
 const title=process.argv[3];
 
-const todo=[]
 
 if(command==="add"){
-    task={
-        id: todo.length() + 1,
-        name:title,
-        complete: false
+    add(title);
     }
-    todo.push(task)}
-    else if
-        (command==="sad"){
-      task={
-        name:'not' +title
-      }
-      
-       todo.push(task)  
-        }
-        else if
-          (command==="dad"){
-            task={
-                name:'papa'+title+'papa'
-            }
-            todo.push(task)
-          }
-          else(
-            console.log("not correct command")
-          )
-        console.log(todo)
+    else if(command==="delete"){
+      delete(title);
+    }
+    else if(command==="list"){
+       list();
+    }
+    else if(command==="done"){
+     done(title);
+    }
+    else{
+      console.log("Not a valid command")
+    }
