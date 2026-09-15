cfb08207e153246c8fa977cea207dfba02e707bf
Rajat Chauhan <rajatchauhan9090@gmail.com>
2026-09-15 21:37:58 +0530

    Update todo storage

--- todo.js
+++ todo.js
@@ -1,50 +1,69 @@
 const process=require('process');
+const fs= require('fs')
 const command=process.argv[2];
 const title=process.argv[3];
 
-const todos = []
+function loadtask(){
+  const data =fs.readFileSync('storage.json','utf-8')
+  return JSON.parse(data)
+}
+
+function save (todos){
+  fs.writeFileSync('storage.json',JSON.stringify(todos))
+}
+
+let todos = loadtask()
 
 function add(title){
   task={
-    Id:todos.length()+1,
+    Id:todos.length+1,
     Name:(title),
     complete:false
   }
  todos.push(task);
+ save(todos)
 }
+
   
-function del(Id){
-  let index=todos.findIndex(function(task){
-    return task.Id==Id;
-  })
-  if(index!==1){
-    todos.splice(index,1)
-  }
+function del(Id) {
+    let index = todos.findIndex(function(task) {
+        return task.Id === Number(Id);
+    });
+
+    if (index === -1) {
+        console.log("not found");
+        return;
+    }
+
+    todos.splice(index, 1);
+    save(todos);
 }
 
 function list(){
   for(i=0;i<todos.length;i++){
-    console.log(todos(i))
+    console.log(todos[i])
   }
 }
-function done(Id){
-  const index=todos.findIndex(function(task){
-    return task.Id===Id
-  })
-  if (!index){
-    console.log("not found")
-  }
-  todos[index].compelete=true;
-    
+function done(Id) {
+    const index = todos.findIndex(function(task) {
+        return task.Id === Number(Id);
+    });
 
-  }
+    if (index === -1) {
+        console.log("not found");
+        return;
+    }
 
+    todos[index].complete = true;
+    save(todos);
+}
+  
 
 if(command==="add"){
     add(title);
     }
     else if(command==="del"){
-      delete(title);
+      del(title);
     }
     else if(command==="list"){
        list();
